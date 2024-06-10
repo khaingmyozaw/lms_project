3939fc31adbd75ee1d85b3e6b5340859abb28f90
khaingmyozaw <kmyozaw.dev@gmail.com>
2024-06-10 23:10:09 +0630

    tailwind installed

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,13 @@
+@extends('index')
+@section('navbar')
+    <div class="container-fluid">
+        <div class="navbar navbar-expand-lg">
+            <a href="#" class="navbar-brand">
+                <img src="{{ asset('images/assets/lms-logo.png') }}" alt="brand">
+            </a>
+            <ul class="navbar-nav">
+                
+            </ul>
+        </div>
+    </div>
+@endsection
